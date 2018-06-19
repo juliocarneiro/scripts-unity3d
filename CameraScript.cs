63f1942b999ac5f79f1e84bb80376f7cd0481370
juliocarneiro <juliocarneiro@gmail.com>
2018-06-18 21:42:07 -0300

    initial

---
+++ CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform pt;
+	public Transform ct;
+	
+	void Update () {
+		ct.position = Vector3.Lerp(
+			ct.position,
+			new Vector3(pt.position.x, ct.position.y, ct.position.z),
+		1f);
+	}
+}
---
+++ Movimento.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimento : MonoBehaviour {
+    public float speed = 3f;
+    public float jumpForce = 200f;
+    private bool facingRight = true;
+    private float movX;
+    private Rigidbody2D rb;
+
+    private bool jumping;
+    private Animator playerAnimator;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        jumping = false;
+        playerAnimator = GetComponent<Animator>();
+	}
+
+    private void FixedUpdate()
+    {
+        Mov();
+        Jump();
+    }
+
+    void Mov()
+    {
+        movX = Input.GetAxis("Horizontal");
+        if (movX > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (movX < 0 && facingRight)
+            Flip();
+
+        rb.velocity = new Vector2(movX * speed, rb.velocity.y);
+
+        if(movX != 0)
+            playerAnimator.SetBool("walk", true);
+        else
+            playerAnimator.SetBool("walk", false);
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = GetComponent<Transform>().localScale;
+        scale.x *= -1;
+        GetComponent<Transform>().localScale = scale;
+    }
+
+    void Jump()
+    {
+        var AbsVelY = Mathf.Abs(rb.velocity.y);
+        jumping = AbsVelY >= 0.05f;
+
+        if(Input.GetKeyDown("up") && !jumping)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+            playerAnimator.SetBool("jump", true);
+        } else if (!jumping)
+        {
+            playerAnimator.SetBool("jump", false);
+        }
+    }
+}
